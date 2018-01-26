fed1b5dfe670f686789370696142e1cbca03ca10
Friedrich Wessel <friedrich.wessel@innogames.com>
2018-01-26 23:19:33 +0100

    Basic Setup + Walk towards ability

---
+++ ggjhacknslay_unity/Assets/Scripts/Abilities/WalkTowardsGoalAbility.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+public class WalkTowardsGoalAbility : MonoBehaviour
+{
+	public float BaseSpeed;
+	
+	private Vector3 _targetPosition;
+	private NavMeshAgent _agent;
+	private float _activeSpeed; 
+	
+	// Use this for initialization
+	[Inject]
+	public void Init ()
+	{
+		_agent = gameObject.GetComponent<NavMeshAgent>();
+		SetSpeed(BaseSpeed);
+	}
+
+	public void SetSpeed(float newSpeed)
+	{
+		_activeSpeed = newSpeed;
+		_agent.speed = _activeSpeed;
+	}
+
+	public void SetTarget(Vector3 target)
+	{
+		_targetPosition = target;
+		_agent.SetDestination(_targetPosition);
+	}
+
+}
---
+++ ggjhacknslay_unity/Assets/Scripts/PlayTests/WalkTowardsTargetTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class WalkTowardsTargetTest  {
+	private NavMeshAgent _agent;
+	private WalkTowardsGoalAbility _ability;
+
+
+	private void CreateSetup()
+	{
+		var go = new GameObject("TestAgent");
+		_agent = go.AddComponent<NavMeshAgent>();
+		_ability = go.AddComponent<WalkTowardsGoalAbility>();
+	}
+
+	[TearDown]
+	public void RunAfterEveryTest()
+	{
+		if (_agent != null)
+		{
+			Object.DestroyImmediate(_agent.gameObject);
+		}
+	}
+
+	[UnityTest]
+	public IEnumerator SetTargetShouldMoveToTarget()
+	{
+		yield return SceneManager.LoadSceneAsync("TestingGround", LoadSceneMode.Additive);
+		CreateSetup();
+		_ability.Init();
+		_ability.SetSpeed(10);
+		_agent.transform.position = new Vector3(0,0,0);
+		_ability.SetTarget(new Vector3(2,0,2));
+		while (!_agent.hasPath)
+		{
+			yield return null;
+		}
+		yield return new WaitForSeconds(1);
+		Assert.IsTrue(_ability.transform.position.x > 1.9 && _ability.transform.position.x < 2.1,
+			string.Format("Expected x>9.9 & <10.1 got:{0}", _ability.transform.position.x));
+		Assert.IsTrue(_ability.transform.position.z > 1.9 && _ability.transform.position.z < 2.1,
+			string.Format("Expected z>9.9 & <10.1 got:{0}", _ability.transform.position.z));
+		yield return SceneManager.UnloadSceneAsync("TestingGround");
+	}
+}
