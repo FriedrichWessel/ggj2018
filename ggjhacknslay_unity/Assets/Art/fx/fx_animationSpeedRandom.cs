8f9f1dae1c7a987de20827beddf503781e3e1524
Bastian Pastoors <b.pastoors@gmail.com>
2018-01-27 14:27:04 +0100

    art art

---
+++ ggjhacknslay_unity/Assets/Art/fx/fx_animationSpeedRandom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fx_animationSpeedRandom : MonoBehaviour {
+
+	public float _min = 0.1f;
+	public float _max = 1.4f;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Animator> ().speed = Random.Range (_min, _max);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
