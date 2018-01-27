011f8ef7731d1766b86cb4809f328e44a171c27e
Friedrich Wessel <friedrich.wessel@innogames.com>
2018-01-27 14:22:30 +0100

    Basic Behaviour Zone System

---
+++ ggjhacknslay_unity/Assets/Scripts/Behaviours/Behaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour
+{
+	public Behaviour NextBehaviour;
+	public virtual void Activate()
+	{
+	}
+
+	public virtual void DeactivateBehaviour()
+	{
+		if (NextBehaviour != null)
+		{
+			NextBehaviour.Activate();
+		}
+	}
+}
