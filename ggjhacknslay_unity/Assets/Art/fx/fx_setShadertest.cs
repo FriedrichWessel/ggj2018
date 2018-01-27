8a65b820731dcf85abb1edf2cd794bc38a5569e6
Bastian Pastoors <b.pastoors@gmail.com>
2018-01-27 17:09:40 +0100

    Art

---
+++ ggjhacknslay_unity/Assets/Art/fx/fx_setShadertest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fx_setShadertest : MonoBehaviour {
+
+	public float _wire = 0f;
+	public float _scramble = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Shader.SetGlobalFloat ("_shdrWireframe", _wire);
+		Shader.SetGlobalFloat ("_shdrScramble", _scramble);
+	}
+}
