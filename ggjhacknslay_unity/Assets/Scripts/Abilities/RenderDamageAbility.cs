2e68803d517ef7ab415bf1097e1aa032bc135ca5
Friedrich Wessel <friedrich.wessel@innogames.com>
2018-01-28 02:27:23 +0100

    Basic get hit render

---
+++ ggjhacknslay_unity/Assets/Scripts/Abilities/RenderDamageAbility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class RenderDamageAbility : Ability
+{
+	public HealthData Data;
+	public Animator Animator;  
+
+	private float _lastSeenData;
+	private bool _firstUpdate = true; 
+	
+	[Inject]
+	void Init()
+	{
+		_lastSeenData = Data.CurrentHealth;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (_firstUpdate)
+		{
+			_lastSeenData = Data.CurrentHealth;
+			_firstUpdate = false;
+		}
+		if (_lastSeenData != Data.CurrentHealth)
+		{
+			_lastSeenData = Data.CurrentHealth;
+			Animator.SetTrigger("Hit");
+		}
+	}
+}
